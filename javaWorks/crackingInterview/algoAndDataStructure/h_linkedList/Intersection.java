ef5840f4ea30997ab844371b79f181504b52864d
oceanvijai <oceanvijai@gmail.com>
2019-11-30 21:38:57 +0530

    sysdesign updates

--- javaWorks/crackingInterview/algoAndDataStructure/h_linkedList/Intersection.java
+++ javaWorks/crackingInterview/algoAndDataStructure/h_linkedList/Intersection.java
@@ -8,12 +8,16 @@ public class Intersection {
      */
 
     /**
-     * 1. Run through each linked list to get the lengths and the tails. 2. Compare
-     * the tails. If they are different (by reference, not by value), return
-     * immediately. There is no intersection. 3. Set two pointers to the start of
-     * each linked list. 4. On the longer linked list, advance its pointer by the
-     * difference in lengths. 5. Now, traverse on each linked list until the
-     * pointers are the same.
+     * 1. Run through each linked list to get the lengths and the tails. 
+     * 
+     * 2. Compare the tails. If they are different (by reference, not by value), 
+     * return immediately. There is no intersection. 
+     * 
+     * 3. Set two pointers to the start of each linked list. 
+     * 
+     * 4. On the longer linked list, advance its pointer by the difference in lengths. 
+     * 
+     * 5. Now, traverse on each linked list until the pointers are the same.
      */
 
     LinkedListNode findIntersection(LinkedListNode list1, LinkedListNode list2) {
