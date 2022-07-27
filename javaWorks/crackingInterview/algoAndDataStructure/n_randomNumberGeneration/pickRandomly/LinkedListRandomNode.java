5d7e27afdc40d5d598f0ff89114479e6fada783c
oceanvijai <oceanvijai@gmail.com>
2022-07-27 22:14:34 +0530

    Update LinkedListRandomNode.java

--- javaWorks/crackingInterview/algoAndDataStructure/n_randomNumberGeneration/pickRandomly/LinkedListRandomNode.java
+++ javaWorks/crackingInterview/algoAndDataStructure/n_randomNumberGeneration/pickRandomly/LinkedListRandomNode.java
@@ -49,27 +49,32 @@ public class LinkedListRandomNode{
 	
     **/
 
-	private ListNode head;
+	ListNode head;
 
-    /** @param head The linked list's head.
-        Note that the head is guaranteed to be not null, so it contains at least one node. */
     public Solution(ListNode head) {
         this.head = head;
     }
-
-    /** Returns a random node's value. */
+    
     public int getRandom() {
-        int scope = 1, chosenValue = 0;
-        ListNode curr = this.head;
-        while (curr != null) {
-            // decide whether to include the element in reservoir
-            if (Math.random() < 1.0 / scope)
-                chosenValue = curr.val;
-            // move on to the next node
-            scope += 1;
-            curr = curr.next;
+        int itemsVistedSoFar = 0;
+        int ans = 0;
+        
+        ListNode currentNode = head;
+        while(currentNode != null){
+            itemsVistedSoFar++;
+            
+            // Choose a random number and bring it to the current bound
+            int rmdInt = (int)(Math.random() * itemsVistedSoFar);
+            
+            // Check if the "rmdInt" is itemsVistedSoFar, if so choose its value as answer
+            // Reservoir sampling
+            if(rmdInt == itemsVistedSoFar-1) ans = currentNode.val;
+            
+            
+            currentNode = currentNode.next;
         }
-        return chosenValue;
+        
+        return ans;
     }
 
 
