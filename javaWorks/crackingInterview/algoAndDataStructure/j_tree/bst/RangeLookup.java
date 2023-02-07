0c418a815f3e0eac4aa60e8037b9b32172b05733
oceanvijai <oceanvijai@gmail.com>
2023-02-07 20:25:29 +0530

    Create RangeLookup.java

---
+++ javaWorks/crackingInterview/algoAndDataStructure/j_tree/bst/RangeLookup.java
@@ -0,0 +1,16 @@
+public class RangeLookup {
+    
+    /**
+      Write a program that takes as input a BST and an interval and returns the BST keys that lie in the interval.
+      
+      If the interval is [5,10], then the function should return all nodes within this range.
+    **/
+  
+  
+    /**
+        We may need to do inorder traversal and pick only those in the range.
+        Time O(n)
+    **/
+
+
+}
