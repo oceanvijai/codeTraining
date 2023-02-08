3aa6e032c02a111ab1508c1a4f4c29e8940d3b93
oceanvijai <oceanvijai@gmail.com>
2023-02-08 15:41:28 +0530

    Create MostVisitedPages.java

---
+++ javaWorks/crackingInterview/algoAndDataStructure/j_tree/bst/MostVisitedPages.java
@@ -0,0 +1,14 @@
+public class MostVisitedPages{
+  
+  
+  /**
+       Write a function to read the next line from a log file, and a function to find the k most visited pages, 
+       where k is an input to the function. Optimize performance for the situation where calls to the two functions are interleaved. 
+       You can assume the set of distinct pages is small enough to fit in RAM
+  **/
+  
+  
+
+
+
+}
