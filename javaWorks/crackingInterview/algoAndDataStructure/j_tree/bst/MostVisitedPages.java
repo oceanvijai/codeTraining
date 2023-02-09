4c5db3096a04e0245aac07d0a84b6d16ea75104f
oceanvijai <oceanvijai@gmail.com>
2023-02-09 23:51:02 +0530

    Update MostVisitedPages.java

--- javaWorks/crackingInterview/algoAndDataStructure/j_tree/bst/MostVisitedPages.java
+++ javaWorks/crackingInterview/algoAndDataStructure/j_tree/bst/MostVisitedPages.java
@@ -8,6 +8,27 @@ public class MostVisitedPages{
   **/
   
   
+  /**
+    As a concrete example, suppose the log file ids appear in the following order: g,a,t,t,a,a,a,g,t,c,t,a,t,i.e., 
+    there are four pages with ids a,c,g,t. After the first 10 lines have been read, 
+    the most common page is a with a count of 4, and the next most common page is t with a count of 3.
+  **/
+  
+  
+  /**
+    First we need to keep the current count of all the nodes
+    Need to udpate the count on the next line read
+    Find the top k pages from the list
+    
+    Solution 1: Brute force: HasMap all the pages and counts. Iterate over them to find the k most visited. Time O(n).
+
+    Solution 2: 
+    We can have an hashmap with the pages count and a tree map to store the count & pages
+    When we add a page we update both maps
+    To get the top K, do head
+  
+  **/
+  
 
 
 
