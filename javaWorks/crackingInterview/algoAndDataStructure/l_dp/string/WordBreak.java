e3caa27e0845b930b493ceaf8159a35dbcb47695
oceanvijai <oceanvijai@gmail.com>
2023-07-31 19:09:10 +0530

    Update WordBreak.java

--- javaWorks/crackingInterview/algoAndDataStructure/l_dp/string/WordBreak.java
+++ javaWorks/crackingInterview/algoAndDataStructure/l_dp/string/WordBreak.java
@@ -19,16 +19,27 @@ public class WordBreak {
                                     = 2^n // This is same via masters therom
     **/
 
+    // Above for recursive solution
+
 
     /**
         Top down
         t(n)  = T(n-m) + T(n-2m) + T(n-3m)... + Km
               = 2T(n-m) + km
               = 2^f T(n-fm) + fkm
-        
               f = (n+1)/m == n/m
-        
               = 2^(n/m) + nk
+
+              When m = 1, we have 2^n
+
+              But if we look closer, we return when we got true from the reursion, which means, T(n) does not 
+              go for T(n-m) , T(n-2m)... so on
+
+              So in worst case, It goes like this
+      T(n) = T(n-m) + km, km - for the dict check and only one goes for the recursion
+
+      Which means we have, T(n) = nk
+              
 
     **/
 
