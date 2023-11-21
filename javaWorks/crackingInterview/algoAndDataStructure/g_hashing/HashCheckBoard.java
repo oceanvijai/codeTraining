5e1415c7591718a9f6b48531ea4953d62d8b55ee
oceanvijai <oceanvijai@gmail.com>
2023-11-21 23:00:12 +0530

    Create HashCheckBoard.java

---
+++ javaWorks/crackingInterview/algoAndDataStructure/g_hashing/HashCheckBoard.java
@@ -0,0 +1,19 @@
+public class HashCheckBoard{
+
+  /**
+      Design a hash function for chess game states. 
+      Your function should take a state and the hash code for that state, and a move, and efficiently compute the hash code for the updated state
+  **/
+
+
+  /**
+      Approach 1: We have 4 types of pieces + 2 colours + 1 empty. so totally 13 values are possible. So we need 4 bits to represent 13 pieces.
+      So, 64 char lenght hashcode can be created with each char with a digit which is base 13. 
+      like 0=empty, 1=blackKing, 2=blackQueen... 9=somePiece, A=somePiece,B&C. 
+
+      Approach 2: 
+  **/
+
+
+  
+}
