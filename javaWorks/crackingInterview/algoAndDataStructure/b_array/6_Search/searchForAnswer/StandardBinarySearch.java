e9e56f489214e1ef4e047676e01b5cc73e5529b9
oceanvijai <oceanvijai@gmail.com>
2024-04-28 00:24:32 +0530

    Update StandardBinarySearch.java

--- javaWorks/crackingInterview/algoAndDataStructure/b_array/6_Search/searchForAnswer/StandardBinarySearch.java
+++ javaWorks/crackingInterview/algoAndDataStructure/b_array/6_Search/searchForAnswer/StandardBinarySearch.java
@@ -10,27 +10,29 @@ public class StandardBinarySearch{
     4. mid - appx mid between start and end
     5. ans - Possible answer until the search is completed
 
+    Note the below one will do the following,
+    If arr=[1,2,3,4,4,4,9,9], searchItem = 4, result = 3; //giving us the lowest index of the search element
+    If arr=[1,2,3,4,4,4,9,9], searchItem = 5, result = 6; //giving us the index where this element can be inserted
+
   **/
 
 
-  public void int binarySearchIndex(int searchItem, int[] arr){
-      int n = arr.length-1;
-      int start= 0, mid = n, ans = n;// depends
+  public static int binarySearchIndex(int searchItem, int[] arr){
+        int n = arr.length-1;
+        int start= 0, end = n, ans = n;// depends
 
-      while(start <= end){
-        int mid = (start + (end-start/2));
-        if(arr[mid] >= searchItem){
-            ans = mid; // possible answer since it has "="
-            end = mid-1;
-        }else{
-          start = mid+1;
+        while(start <= end){
+            int mid = (start + (end-start/2));
+            if(arr[mid] >= searchItem){
+                ans = mid; // possible answer since it has "="
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
         }
-      }
 
-      return arr[ans];
-  }  
-
-  
+        return ans;
+    }
 
   
 }
